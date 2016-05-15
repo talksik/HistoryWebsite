314522d800e7369c2e683079c8d75be2caf58973
talksik <patel.arjun50@gmail.com>
2016-05-14 18:58:21 -0700

    Update niggascript.js

--- history website/niggascript.js
+++ history website/niggascript.js
@@ -1,15 +1,34 @@
 $(window).scroll(
     {
-        previousTop: 100%
+        previousTop: 900
     },
     function () {
     var currentTop = $(window).scrollTop();
     if (currentTop < this.previousTop) {
         $(".sidebar em").text("Up"); /* optional for demo */
-        $(".header").show();
-    } else {
+      $(".header").show();
+    }
+    else  {
         $(".sidebar em").text("Down");
         $(".header").hide();
     }
-    this.previousTop = currentTop;
+      this.previousTop = currentTop;
+});
+$(function(){
+    $('a[href^="#"]').click(function(e){
+          var target = $(this).attr('href');
+          var strip = target.slice(1);
+          var anchor = $("a[name='"+  strip  +"']");
+
+          e.preventDefault();
+
+          $('html, body').animate({
+
+            scrollTop: anchor.offset().top
+
+
+          }, 'slow');
+
+    });
+
 });
