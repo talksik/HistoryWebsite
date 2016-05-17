6e2d543321e3b19459c6423f22ce28e641ff0eef
talksik <patel.arjun50@gmail.com>
2016-05-17 01:16:26 -0700

    Update niggascript.js

--- history website/niggascript.js
+++ history website/niggascript.js
@@ -1,19 +1,41 @@
+
+/*----------------loading screen disappears---------*/
+
+
+$(window).load(function(){
+    $(".loader").fadeOut(4000);
+
+});
+
+
+
+/*---------hide and showing of navbar with scroll up and down-----*/
+
 $(window).scroll(
     {
-        previousTop: 900
+        previousTop: 0
     },
     function () {
     var currentTop = $(window).scrollTop();
-    if (currentTop < this.previousTop) {
+    if (currentTop < 900) {
         $(".sidebar em").text("Up"); /* optional for demo */
-      $(".header").show();
+      $(".header").show("fast");
     }
+    else if (currentTop < this.previousTop) {
+          $(".sidebar em").text("Up"); /* optional for demo */
+        $(".header").show("fast");
+      }
     else  {
         $(".sidebar em").text("Down");
-        $(".header").hide();
+        $(".header").hide("fast");
     }
       this.previousTop = currentTop;
 });
+
+
+
+/*-----------Smooth Scroll from clicking on link in navbar-----*/
+
 $(function(){
     $('a[href^="#"]').click(function(e){
           var target = $(this).attr('href');
@@ -30,5 +52,31 @@ $(function(){
           }, 'slow');
 
     });
+
+});
+
+
+
+/*-------------Parallax--------------*/
+
+
+
+$(window).scroll(function(){
+
+    var wScroll = $(this).scrollTop();
+
+    $('.cover').css({
+      'transform' : 'translate(0px, '+ wScroll /10 +'%)'
+    });
+
+    $('.cover1').css({
+      'transform' : 'translate(0px, -'+ wScroll /15 +'%)'
+    });
+
+    $('.music').css({
+      'transform' : 'translate(0px, -'+ wScroll /15 +'%)'
+    });
+
+
 
 });
